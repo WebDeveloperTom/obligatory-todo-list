c77eb096dbba0125948b722f33d30f18344f05f4
WebDeveloperTom <tom@webdevelopertom.com>
2019-01-05 22:26:53 +1300

    update completed styling

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 
 class ToDoItem extends Component {
+  getStyle = () => {
+    return {
+      textDecoration: this.props.info.complete ? "line-through" : "none"
+    };
+  };
   render() {
     return (
       <div>
-        <li>{this.props.info.title}</li>
+        <li style={this.getStyle()}>{this.props.info.title}</li>
       </div>
     );
   }
